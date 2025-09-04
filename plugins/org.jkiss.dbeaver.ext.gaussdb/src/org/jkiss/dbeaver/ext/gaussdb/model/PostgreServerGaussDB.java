8259da60371580bf9afff077662c314627877a49
acewuye <1441256632@qq.com>
2025-09-04 14:29:42 +0800

    #37811 Resolve the 'Unsupported Vector Type' error

--- plugins/org.jkiss.dbeaver.ext.gaussdb/src/org/jkiss/dbeaver/ext/gaussdb/model/PostgreServerGaussDB.java
+++ plugins/org.jkiss.dbeaver.ext.gaussdb/src/org/jkiss/dbeaver/ext/gaussdb/model/PostgreServerGaussDB.java
@@ -17,6 +17,8 @@
 
 package org.jkiss.dbeaver.ext.gaussdb.model;
 
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.ext.gaussdb.GaussDBConstants;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDatabase;
 import org.jkiss.dbeaver.ext.postgresql.model.impls.PostgreServerExtensionBase;
@@ -61,5 +63,11 @@ public class PostgreServerGaussDB extends PostgreServerExtensionBase {
     @Override
     public PostgreDatabase.SchemaCache createSchemaCache(PostgreDatabase database) {
         return new GaussDBSchemaCache();
+    }
+
+    @Override
+    public boolean isPGObject(@NotNull Object object) {
+        String className = object.getClass().getName();
+        return GaussDBConstants.GAUSSDB_PG_OBJECT_CLASS.equals(className);
     }
 }
